e288dee4dcf6fd613ad62db31cccb84a03783d67
ha2shin2 <ha2shin2@naver.com>
2024-04-28 15:16:31 +0900

    implement json load and store

--- NicknameCapture/Scripts/mouse_test.py
+++ NicknameCapture/Scripts/mouse_test.py
@@ -5,77 +5,100 @@ import pyautogui as pygui
 import json
 
 class Region:
-    def __init__(self, start_pos, end_pos):
-        self.name = ""
-        self.region = [0, 0, 0, 0]
-        self.region[0] = start_pos[0] #x1
-        self.region[1] = start_pos[1] #y1
-        self.region[2] = end_pos[0] #x2
-        self.region[3] = end_pos[1] #y2
+    def __init__(self, name, start_pos, end_pos):
+        self.__name = name
+        self.__region = [0, 0, 0, 0]
+        self.__region[0] = start_pos[0] #x1
+        self.__region[1] = start_pos[1] #y1
+        self.__region[2] = end_pos[0] #x2
+        self.__region[3] = end_pos[1] #y2
 
     def get_width(self):
-        return abs(self.region[0] - self.region[2])
+        return abs(self.__region[0] - self.__region[2])
 
     def get_height(self):
-        return abs(self.region[1] - self.region[3])
+        return abs(self.__region[1] - self.__region[3])
 
     def get_dict(self):
-        return {"name" : self.name, "region" : self.region}
+        return {"name" : self.__name, "region" : self.__region}
 
     def set_name(self, name):
-        self.name = name
-          
-region_list = []
+        self.__name = name
 
-def save_json(json_data):
-    file_path = "./Regions.json"
-    try:
-        with open(file_path, "r") as json_file:
-            prev_data = json.load(json_file)        
-    except FileNotFoundError:
-        #when no file
-        with open(file_path, 'w') as outfile:
-            json.dump(json_data, outfile, indent=2)
+    def get_name(self):
+        return self.__name
+    
+    def get_region(self):
+        return self.__region
+
+class NicknameCapture:
+    def __init__(self):
+        self.__start_pos = []
+        self.__end_pos = []
+        self.__file_path = "./Regions.json"
+        self.__region_list= []
+        self.__load_json()
+
         
-    except json.decoder.JSONDecodeError:
-        #when wrong json
-        print("WRONG JSON FILE!")
-    else:
-        #insert mouse position data    
-        new_data = prev_data + json_data
-        with open(file_path, 'w') as outfile:
-            json.dump(new_data, outfile, indent=2)
+    def __load_json(self):
+        try:
+            with open(self.__file_path, "r") as json_file:
+                json_data = json.load(json_file)
+        except FileNotFoundError:
+            print("File not exist")
+        except json.decoder.JSONDecodeError:
+            #when wrong json
+            print("WRONG JSON FILE!")
+        else:
+            for region in json_data:
+                self.__region_list.append(Region(region["name"],\
+                                          [region["region"][0], region["region"][1]],\
+                                          [region["region"][2], region["region"][3]]))
+                
+            for region in self.__region_list:
+                print(region.get_name(), region.get_region())
 
-def on_click(x, y, button, pressed):
-    print('on_click')
-    if pressed:
-        global start_pos
-        start_pos = [x, y]
-    if not pressed:
-        global end_pos
-        end_pos = [x, y]
-        region_list.append(Region(start_pos, end_pos))
+    def start_hotkey(self):
+        with HotKeys({
+        '<shift>+d': self.on_activate_drag,
+        '<shift>+s': self.on_save_json}) as h:
+            h.join()
+
+    def on_click(self, x, y, button, pressed):    
+        if pressed:
+            print('on_pressed')
+            self.__start_pos.append(x)
+            self.__start_pos.append(y)
+        if not pressed:
+            print('on_release')
+            self.__end_pos.append(x)
+            self.__end_pos.append(y)
+            self.__region_list.append(Region("", self.__start_pos, self.__end_pos))
+            return False
+
+    
+    def on_activate_drag(self):
+        #shift + d
+        print('on_act_start')
+        with Listener(on_click=self.on_click) as l:
+            l.join()
+
         return False
 
-def on_activate_drag():
-    #shift + d
-    print('on_act_start')
-    with Listener(on_click=on_click) as l:
-        l.join()
+    def on_save_json(self):
+        #shift + s
+        print('on_save')
+        json_data = []
+        for reg in self.__region_list:
+            dic = reg.get_dict()
+            json_data.append(dic)
 
-def on_save_json():
-    #shift + s
-    print('on_save')
-    json_data = []
-    for reg in region_list:
-        dic = reg.get_dict()
-        json_data.append(dic)
-        print(json_data)
+        with open(self.__file_path, 'w') as outfile:
+            json.dump(json_data, outfile, indent=2)
 
-    save_json(json_data)
+        return False
 
-with HotKeys({
-        '<shift>+d': on_activate_drag,
-        '<shift>+s': on_save_json}) as h:
-    h.join()
+
+a=NicknameCapture()
+a.start_hotkey()
   
