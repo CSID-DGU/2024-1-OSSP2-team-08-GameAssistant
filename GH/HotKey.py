b6cbf14fa9159cb782b0cdb301e41ceeace9ab2d
ha2shin2 <ha2shin2@naver.com>
2024-05-28 21:12:31 +0900

    function on release

--- GH/HotKey.py
+++ GH/HotKey.py
@@ -1,21 +1,19 @@
 from pynput.keyboard import Listener, Key, KeyCode
- 
+
 store = set()
 hot_key = set([Key.alt_l, KeyCode(char='s')])
 
 def handleKeyPress( key ):
     store.add( key )
-    print('\x13' in store)
-    check = all([True if k in store else False for k in hot_key ])
-    print(store)
-    print([True if k in store else False for k in hot_key ])
-    if check:
-        return False # do something
  
-def handleKeyRelease( key ): 
+def handleKeyRelease( key ):
+    check = all([True if k in store else False for k in hot_key ])
+    if check:
+        return False #DoSomething
+        
     if key in store:
         store.remove( key )
-        
+
     # 종료
     if key == Key.esc:
         return False
