07337d140a866d2266509455c82b3d340632f9c9
ha2shin2 <ha2shin2@naver.com>
2024-06-05 23:11:07 +0900

    MMRUI Before all userStats

--- Scripts/MMRUI.py
+++ Scripts/MMRUI.py
@@ -1,5 +1,6 @@
 import sys, os
 import json
+import api
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
@@ -7,25 +8,27 @@ from PyQt5.QtWidgets import QWidget
 from PyQt5.QtGui import QPixmap
 #from mmrUItest import mmrUI
 
-mmrUisrc = uic.loadUiType("Data/UI/MMRUI/mmrUI.ui")[0]
-MMRUIWindowSource = uic.loadUiType("Data/UI/MMRUI/MMRUI3P.ui")[0]
-MMRJsonPath = "Data/TestJson/playersMMR/Player"
+mmrUisrc = uic.loadUiType("../Data/UI/MMRUI/mmrUI.ui")[0]
+MMRUIWindowSource = uic.loadUiType("../Data/UI/MMRUI/MMRUI3P.ui")[0]
 
 class MMRWindowUI(QMainWindow, MMRUIWindowSource):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.initialized = False
 
-    def initialize(self):
-        if not self.initialized:
-            for i in range(1, 4):
-                self.AddMMRFrame(MMRJsonPath+str(i)+".json")
-            self.initialized = True
+    def UpdateData(self , nickname_list):
+        for nickname in nickname_list:
+            playerMMRData = API_GetPlayerMMRInfo(nickname)
 
-    def AddMMRFrame(self, jsonPath):
+            if playerMMRData == None:
+                #404
+                return
+            self.AddMMRFrame(playerMMRData)
+        
+
+    def AddMMRFrame(self, jsonObj):
         mmrFrame = mmrUI()
-        mmrFrame.setInfo(jsonPath)
+        mmrFrame.SetInfo(jsonObj)
         self.MMRLayout.addWidget(mmrFrame)
 
 class mmrUI(QWidget, mmrUisrc):
@@ -33,14 +36,10 @@ class mmrUI(QWidget, mmrUisrc):
         super().__init__()
         self.setupUi(self)
         
-    def setInfo(self, filepath):
-        with open(filepath, 'r', encoding='utf-8') as f:
-            jsonObj = json.load(f)
-
-
+    def SetInfo(self, jsonObj):
         # 첫 번째 유저의 데이터 가져오기
-        user_stats = jsonObj["userStats"]
-        Charactercode = user_stats["characterStats"]["characterCode"]
+        user_stats = jsonObj["userStats"][0]
+        Charactercode = user_stats["characterStats"][0]["characterCode"]
         Nickname = user_stats["nickname"]
         rankPoint = user_stats["mmr"]
         rankNum = user_stats["rank"]
@@ -51,7 +50,7 @@ class mmrUI(QWidget, mmrUisrc):
         useravgAssistant = user_stats["averageAssistants"]
         
         # 첫 번째 캐릭터의 데이터 가져오기
-        char_stats = user_stats["characterStats"]
+        char_stats = user_stats["characterStats"][0]
         charUsage = char_stats["usages"]
         charTop3 = char_stats["top3"]
         chargameRank = char_stats["averageRank"]
@@ -70,10 +69,24 @@ class mmrUI(QWidget, mmrUisrc):
         self.charusage.setText("플레이 횟수: " + str(charUsage))
         self.charIntop3.setText("In top3: " + str(charTop3))
         self.charavgRank.setText("평균 순위: " + str(chargameRank))
+#region Class End
+
+#region function
+def API_GetPlayerMMRInfo(playerID):
+    factory = api.APIFactory()
+    seasonId = factory.get_current_seasonId()
+    if seasonId == None:
+        return PlayerJsonInfo.default
+        
+    data=factory.get_user_data(nickname=playerID, seasonId=seasonId)
+    if data == None:
+        return None
+    else:
+        return data
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MMRWindowUI()
-    window.initialize()
+    window.UpdateData(['한동그라미', '삼다수는맛있어', '커리'])
     window.show()
     sys.exit(app.exec_())
